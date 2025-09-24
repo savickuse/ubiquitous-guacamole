056ae80f7eaf39f60f02e6f02184afd04f42efdf
savickuse <savickuse@users.noreply.github.com>
2025-09-24 09:09:59 +0300

    feat: build configuration update

--- src/api.js
+++ src/api.js
@@ -131,3 +131,30 @@ class API {
 
 // Update 64
 module.exports = API;
+
+
+// API module for GuacamoleCloud
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 71
+module.exports = API;
