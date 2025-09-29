6952a9eb6ee5e4cae3ef523a0fede1864ab1bf9e
savickuse <savickuse@users.noreply.github.com>
2025-09-30 02:21:41 +0300

    add(tests): security enhancement

--- src/api.js
+++ src/api.js
@@ -77,3 +77,30 @@ class API {
 
 // Update 43
 module.exports = API;
+
+
+// API module for GuacamoleCloud
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 62
+module.exports = API;
