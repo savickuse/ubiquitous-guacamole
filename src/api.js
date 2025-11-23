dd6ace79562b2a2fdce26488c35c95effee19f72
savickuse <savickuse@users.noreply.github.com>
2025-11-23 02:48:45 +0200

    add: update API documentation

--- src/api.js
+++ src/api.js
@@ -23,3 +23,30 @@ class API {
 
 // Update 12
 module.exports = API;
+
+
+// API module for GuacamoleCloud
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 13
+module.exports = API;
